92bacbae906ff86854cd38123808c4fdfa49e478
KekeliVa <kekeli.afantchao@yahoo.com>
2020-09-02 13:50:59 -0400

    developed server.js and burgers_controller

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const router = require("./controller/burgers_controller");
+const PORT = process.env.PORT || 3000;
+
+// Set handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Middleware
+app.use(express.static("public"));
+app.use(exphbs);
+
+app.use("/", router);
+
+
+// Listen to the port
+app.listen(PORT, () => {
+  console.log(`App is currently listening at localhost:${PORT}`);
+});
